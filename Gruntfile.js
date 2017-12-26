e4d275dcd04b7dda83b2659d9b6bea2c244c9ce5
small_sunshine <jioo0224@naver.com>
2017-12-26 19:28:44 +0900

    웹폰트 배포와 관련된 수정사항

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,27 +19,58 @@ module.exports = (grunt) => {
             options: {
                 sourceMap: true
             },
-            concat: {
-                src: ['src/css/main.css', 'src/css/service/*.css'],
+            svg: {
+                src: ['src/css/main.css', 'src/css/svg/*.css'],
                 dest: 'dist/css/oauth-buttons.css'
+            },
+            font: {
+                src: ['src/css/main.font.css', 'src/css/font/*.css'],
+                dest: 'dist/css/oauth-buttons-font.css'
             }
         },
         copy: {
-            copy: {
+            fonts: {
               files: [{
                     expand: true,
-                    cwd: 'src/logo',
-                    src: '*.svg',
-                    dest: 'dist/logo',
+                    cwd: 'src/fonts',
+                    src: '*',
+                    dest: 'dist/fonts',
                 }],
             },
+        },
+        svgo: {
+            svgo: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/logo',
+                    src: ['*.svg','!*_solid.svg'],
+                    dest: 'dist/logo'
+                }]
+            }
+        },
+        'convert-svg-to-png': {
+            svg: {
+                options: {
+                    size: {
+                        w: '64px',
+                        h: '64px'
+                    },
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/logo',
+                    src: ['*.svg'],
+                    dest: 'dist/logo'
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-webfont');
+    grunt.loadNpmTasks('grunt-svgo');
+    grunt.loadNpmTasks('grunt-convert-svg-to-png');
 
-    grunt.registerTask('default', ['concat', 'copy', 'cssmin']);
+    grunt.registerTask('default', ['concat', 'copy', 'svgo', 'convert-svg-to-png', 'cssmin']);
 }
